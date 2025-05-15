6526c65cc8f0be964cfa1ca7cf724e6718c35c02
Şahin Karaoğlu <76259114+sahinkaraoglu@users.noreply.github.com>
2025-05-15 03:34:45 +0300

    Update

--- RestERP.Application/obj/Debug/net8.0/RestERP.Application.AssemblyInfo.cs
+++ RestERP.Application/obj/Debug/net8.0/RestERP.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestERP.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87673b6f942346a844b7f0fd650918060dab8af0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bae40e46e69382293ddb2fa570c62c6fe37de7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestERP.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestERP.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
