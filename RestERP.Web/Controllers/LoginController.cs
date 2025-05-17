bfbeeede0b42dac8f9a295517779a553eb2ad632
Şahin Karaoğlu <76259114+sahinkaraoglu@users.noreply.github.com>
2025-05-17 17:45:04 +0300

    LoginController Added

---
+++ RestERP.Web/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using RestERP.Web.Models;
+
+namespace RestERP.Web.Controllers;
+
+public class LoginController : Controller
+{
+    private readonly ILogger<LoginController> _logger;
+
+    public LoginController(ILogger<LoginController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Panel()
+    {
+        return View();
+    }
+
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
