cf7022b41f73caf128582e54c00c2c329cfdfc2b
Şahin Karaoğlu <76259114+sahinkaraoglu@users.noreply.github.com>
2025-05-30 23:55:03 +0300

    TableId Update

---
+++ RestERP.Infrastructure/Migrations/20250530204936_Migration7.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestERP.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration7 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_TableId",
+                table: "Orders",
+                column: "TableId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Tables_TableId",
+                table: "Orders",
+                column: "TableId",
+                principalTable: "Tables",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Tables_TableId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_TableId",
+                table: "Orders");
+        }
+    }
+}
