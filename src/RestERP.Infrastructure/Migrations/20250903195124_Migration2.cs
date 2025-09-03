abb1889aa5d20bde975fb132068b280970fd0d25
Şahin Karaoğlu <76259114+sahinkaraoglu@users.noreply.github.com>
2025-09-03 22:51:57 +0300

    New MG

---
+++ src/RestERP.Infrastructure/Migrations/20250903195124_Migration2.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestERP.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "deneme",
+                table: "Tables");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "deneme",
+                table: "Tables",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "Tables",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "deneme",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Tables",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "deneme",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Tables",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "deneme",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Tables",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "deneme",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Tables",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "deneme",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Tables",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "deneme",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Tables",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "deneme",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Tables",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "deneme",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Tables",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "deneme",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Tables",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "deneme",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Tables",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "deneme",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Tables",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "deneme",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Tables",
+                keyColumn: "Id",
+                keyValue: 13,
+                column: "deneme",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Tables",
+                keyColumn: "Id",
+                keyValue: 14,
+                column: "deneme",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Tables",
+                keyColumn: "Id",
+                keyValue: 15,
+                column: "deneme",
+                value: false);
+        }
+    }
+}
